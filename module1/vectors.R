d9c10274b9135bc7d1f0b9e22cadd40d1a4e98fe
Inna <innayarantseva@gmail.com>
2017-03-26 15:00:46 +0700

    Update vectors.R

--- module1/vectors.R
+++ module1/vectors.R
@@ -1,3 +1,6 @@
+#Function gets two arguments (m, n), returns a vector consist of {i/m, i = 0, 1, ..., m} and {j/n, j = 0, 1, ..., n}.
+#No repeats, in decreasing order.
+
 get_fractions <- function(m, n) {
   a <- (0:m) / m
   b <- (0:n) / n
